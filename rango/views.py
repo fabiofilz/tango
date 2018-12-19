1b54684451c1a0273540a4dddee60c971dbb290b
Fabio de Simoni <fabiofilz@gmail.com>
2018-12-19 17:23:28 +0000

    About page

--- rango/views.py
+++ rango/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 
-
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Working")
+    return HttpResponse("Working! <br><a href='/rango/about/'>About</a>")
+    
+def about(request):
+    return HttpResponse("<a href='/rango/'>Index</a>")
